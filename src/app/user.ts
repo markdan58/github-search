7f979bb9498f743cdb3af0ee7f47bc4b8a826f06
markdan58 <mark.dan@strathmore.edu>
2018-10-29 08:25:23 +0300

    added user public details

---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  constructor(
+    public name: string,
+    public login: string,
+    public avatar_url: string,
+    public email: string,
+    public public_repos: number,
+    public html_url: string
+  ) {}
+}
