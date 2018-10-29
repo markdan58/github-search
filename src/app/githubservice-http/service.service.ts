e76019657072e440c96a39418596379cdc33dfbd
markdan58 <mark.dan@strathmore.edu>
2018-10-29 08:29:56 +0300

    added user and repository to class

--- src/app/githubservice-http/service.service.ts
+++ src/app/githubservice-http/service.service.ts
@@ -3,3 +3,13 @@ import { HttpClient } from '@angular/common/http'
 import { environment } from '../../environments/environment'
 import { Repository } from '../repository'
 import { User } from '../user';
+
+
+@Injectable()
+export class githubservice {
+
+  repository: Repository;
+
+  user: User;
+  private username: string;
+  items;
