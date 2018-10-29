57d633618d5399f29f2a649b0c4ada183429dfcd
markdan58 <mark.dan@strathmore.edu>
2018-10-29 08:47:19 +0300

    added component and githubservice

--- src/app/github-search-form/github-search-form.component.ts
+++ src/app/github-search-form/github-search-form.component.ts
@@ -2,5 +2,24 @@ import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { githubservice } from "../githubservice-http/service.service";
 
+@Component({
+  selector: "app-github-search-form",
+  templateUrl: "./github-search-form.component.html",
+  styleUrls: ["./github-search-form.component.css"],
+  providers: [githubservice]
+})
+export class GithubSearchFormComponent implements OnInit {
+  submitSearch(name) {
+    this.githubservice.getgithubSearch(name.target.value);
+    this.repositoryService.getRepository(name.target.value);
+  }
+
+  constructor(
+    private githubservice: githubservice,
+    private repositoryService: githubservice
+  ) {}
+
+  ngOnInit() {}
+}
 
 
