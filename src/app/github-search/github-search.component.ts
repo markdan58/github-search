61db7ac5068b51a73546148138b80ca50c342755
markdan58 <mark.dan@strathmore.edu>
2018-10-29 08:43:04 +0300

    added components

--- src/app/github-search/github-search.component.ts
+++ src/app/github-search/github-search.component.ts
@@ -4,5 +4,26 @@ import { githubservice } from "../githubservice-http/service.service";
 import { Repository } from '../repository'
 
 
+@Component({
+  selector: "app-github-search",
+  templateUrl: "./github-search.component.html",
+  styleUrls: ["./github-search.component.css"],
+  providers: [githubservice]
+})
+export class GithubSearchComponent implements OnInit {
+  user: User;
+  repository: Repository;
+
+  constructor(
+    private githubservice: githubservice,
+    public repositoryService: githubservice
+  ) {
+    this.user = this.githubservice.user;
+  }
+
+  ngOnInit() { }
+}
+
+
 
 
